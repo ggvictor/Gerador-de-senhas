910f2e12d058d0746c69671941dcf277a90f2cea
ggvictor <victorrodriguessilva2@gmail.com>
2024-05-28 16:22:56 -0300

    primeira versão

--- js/scripts.js
+++ js/scripts.js
@@ -15,9 +15,36 @@ const getSymbol = ()=>{
     const symbol ="()[]{}!@#$%¨&*_=++*/.;,?°"
     return symbol[Math.floor(Math.random() * symbol.length)]
 }
-console.log(getNumber())
+
+const generatePassword = (getterLowerCase,getterUpperCase,getNumber,getSymbol) =>{
+    let password = "";
+
+    const passwordLength = 10;
+
+    const generators = [
+        getterLowerCase,
+        getterUpperCase,
+        getNumber,
+        getSymbol
+
+    ]
+    for(i = 0; i < passwordLength; i = i + 4 ){
+        generators.forEach(()=>{
+            const randomValue = generators[Math.floor(Math.random() * generators.length)]();
+            password += randomValue
+        })
+    }
+    password = password.slice(0, passwordLength);
+    generatedPasswordElement.style.display = "block";
+    generatedPasswordElement.querySelector("h4").innerText = password
+};
 
 // eventos
 generatePasswordButton.addEventListener("click", ()=>{
-    console.log("teste")
+    generatePassword(
+        getterLowerCase,
+        getterUpperCase,
+        getNumber,
+        getSymbol
+    );
 })
